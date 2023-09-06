5cb11d4eb16c9190d9577af84431ef280da12677
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2023-09-06 17:03:43 -0300

    cria interface typescript para tipagems dos parâmetros a serem recebidos e mudanças na estilização

--- src/components/box.tsx
+++ src/components/box.tsx
@@ -1,7 +1,11 @@
 'use client'
 
-export function Letter () {
+interface BoxProps {
+  valor: string,
+}
+
+export function Box ({valor}:BoxProps) {
   return (
-    <h1>componente</h1>
+    <div className="border-4 border-black text-black w-36 h-36 flex justify-center items-center">{valor}</div>
   )
 }
