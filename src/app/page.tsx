ddc62c77cb808f934c618f9825657a1b0e895f3a
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2023-09-06 16:32:18 -0300

    ativa o CSR e importa useState do 'react'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,12 @@
+'use client'
+
+import { useState } from "react"
+
 export default function Home() {
+
+  const [jogadorDaVez, setJogadorDaVez] = useState('')
+
+
   return (
     <h1>Jogo da véia</h1>
   )
