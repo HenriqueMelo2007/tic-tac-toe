a683e487124e526bc13ae99def45d9739b8d8857
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2023-09-06 16:37:44 -0300

    cria um novo estado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,8 @@ import { useState } from "react"
 
 export default function Home() {
 
-  const [jogadorDaVez, setJogadorDaVez] = useState('')
+  const [jogadorDaVez, setJogadorDaVez] = useState('X') // 'X' ou 'O'
+  const [quadrados, setQuadrados] = useState(['', '', '', '', '', '', '', '', '']) // 'X' ou 'O'
 
 
   return (
