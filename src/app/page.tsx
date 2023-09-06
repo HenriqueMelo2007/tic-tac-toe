1e9a2051862e306388c30744eae770d61991f47e
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2023-09-06 17:01:27 -0300

    cria visualmente o jogo da velha

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,22 @@
 'use client'
 
+import { Box } from "@/components/box"
 import { useState } from "react"
 
 export default function Home() {
 
   const [jogadorDaVez, setJogadorDaVez] = useState('X') // 'X' ou 'O'
-  const [quadrados, setQuadrados] = useState(['', '', '', '', '', '', '', '', '']) // 'X' ou 'O'
+  const [quadrados, setQuadrados] = useState(['X', 'X', 'X', 'X', 'X', 'X', 'O', 'O', 'O']) // 'X' ou 'O'
 
 
   return (
     <div className="h-screen w-screen flex flex-col items-center justify-center">
       <h1 className="text-5xl">Jogo da velha</h1>
+
+      <div className="w-full grid grid-cols-colunaResponsiva px-padding-customizado justify-center mt-10">
+        {quadrados.map( (item, index) => <Box key={index} valor={item}></Box>)}
+      </div>
+
     </div>
   )
 }
