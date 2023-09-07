04e912ab7fcc159ffe58eeb04a761f6903e40969
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2023-09-07 09:49:10 -0300

    altera os valores numéricos para que sejam compatíveis com o index do array

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
 
   const [jogadorDaVez, setJogadorDaVez] = useState('X')
   const [valoresQuadrados, setValoresQuadrados] = useState(['', '', '', '', '', '', '', '', ''])
-  const [posicoesVencedoras, setPosicoesVencedoras] = useState([[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]])
+  const [posicoesVencedoras, setPosicoesVencedoras] = useState([[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]])
 
 
   return (
